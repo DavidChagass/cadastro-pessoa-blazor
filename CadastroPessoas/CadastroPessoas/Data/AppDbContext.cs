9ef7c184daceae2681973b72b344b28a61cb27ce
David <147016411+DavidChagass@users.noreply.github.com>
2024-07-31 14:08:53 -0300

    Add files via upload

---
+++ CadastroPessoas/CadastroPessoas/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CadastroPessoas.Models;
+using Microsoft.EntityFrameworkCore;
+namespace CadastroPessoas.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        DbSet<Pessoa> Pessoas = null;
+
+
+    }
+}
